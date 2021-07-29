8c0086db396ed549066f2331eb83b8c8526cbd9b
Chris Telford <christelford@hotmail.co.uk>
2021-07-29 16:15:33 +0100

    aborted attempt

--- src/homepage.js
+++ src/homepage.js
@@ -1,22 +1,32 @@
-import kitchenStockIMG from './kitchen.jpg';
+import {createFooter} from './footer.js'
+import {addHeader} from './header.js'
+import kitchenStockIMG from './kitchenM.jpg';
 
 function loadHomePage() {
-    const title = document.createElement('h1');
-    title.textContent = 'Welcome to The Restaurant';
-    
-    const blurb = document.createElement('p');
-    blurb.textContent = 'This is a restaurant that sells great food and drink.'.repeat(4);
+    const mainSection = createElement('div');
 
-    const kitchenIMG = document.createElement('img');
-    // Photo by Michael Browning
-    // https://unsplash.com/@michaelwb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText
-    kitchenIMG.src = kitchenStockIMG;
-    kitchenIMG.alt = "kitchen scene";
+    const subHeader = document.createElement('h2');
+    subHeader.textContent = 'About';
+    subHeader.className = 'blurb';
+
+    const blurb = document.createElement('p');
+    blurb.textContent = 'This is a restaurant that sells great food and drink. '.repeat(10);
+    blurb.className = 'blurb';
+    blurb.style.display = 'grid';
+    
+    // subHeader.style.display = 'grid';
+
+    // const kitchenIMG = document.createElement('img');
+    // // Photo by Michael Browning
+    // // https://unsplash.com/@michaelwb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText
+    // kitchenIMG.src = kitchenStockIMG;
+    // kitchenIMG.id = 'kitchenIMG';
+    // kitchenIMG.alt = "kitchen scene";
 
     const content = document.getElementById('content');
-    content.appendChild(title);
+    content.appendChild(subHeader);
     content.appendChild(blurb);
-    content.appendChild(kitchenIMG);
+
 }
 
 export {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import {loadHomePage} from './homepage.js';
+import { addHeader } from './header.js';
+import { addFooter } from './footer.js';
+import { loadHomePage } from './homepage.js';
+import './reset.css';
 import './style.css';
 
+const fuggleFont = document.createElement('link');
+fuggleFont.setAttribute('href', "https://fonts.googleapis.com/css2?family=Fuggles&display=swap");
+fuggleFont.setAttribute('rel', "stylesheet");
+document.head.appendChild(fuggleFont);
 
+const content = document.createElement('div');
+content.id = 'content';
+document.body.appendChild(content);
+
+// loadContent();
+addHeader();
 loadHomePage();
+addFooter();
